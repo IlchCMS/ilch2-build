bad086978faad96ddb71f542c8fd76b3b89a6ef8
Mairu <mairu@ilch.de>
2016-10-29 14:02:13 +0200

    replace authentication with signature validation

--- index.php
+++ index.php
@@ -15,34 +15,34 @@ if ('POST' === $_SERVER['REQUEST_METHOD']) {
         exit;
     }
 
+    $headers = apache_request_headers();
+
+    if (!isset($headers['Signature'])) {
+        header('HTTP/1.1 401 Unauthorized');
+        logRequest(__LINE__, 'missing Signature header');
+        exit;
+    }
+
+    //load config from travis
+    $travisConfigJson = file_get_contents('https://api.travis-ci.org/config');
+    $travisConfig = json_decode($travisConfigJson, true);
+
+    $verification = openssl_verify(
+        trim($_POST['payload']),
+        base64_decode($headers['Signature']),
+        $travisConfig['config']['notifications']['webhook']['public_key']
+    );
+    if (1 !== $verification) {
+        header('HTTP/1.1 401 Unauthorized');
+        logRequest(__LINE__, $verification === 0 ? 'provided Signature is invalid' : 'error while Signature validation');
+        exit;
+    }
+
     $payload = json_decode($_POST['payload'], true);
 
     if (!isset($payload['repository']['owner_name']) || !isset($payload['repository']['name'])) {
         header('HTTP/1.1 400 Bad Request');
         logRequest(__LINE__, 'missing owner or/and repository name');
-        exit;
-    }
-
-    $headers = apache_request_headers();
-
-    if (!isset($headers['Authorization'])) {
-        header('HTTP/1.1 401 Unauthorized');
-        logRequest(__LINE__, 'missing authorization header');
-        exit;
-    }
-
-    $possibleTokens = array();
-
-    if (!empty($config['fullToken'])) {
-        $possibleTokens[] = $config['fullToken'];
-    }
-
-    $repoSlug = $payload['repository']['owner_name'] . '/' . $payload['repository']['name'];
-    $possibleTokens[] = hash('sha256', $repoSlug . $config['token']);
-
-    if (!in_array($headers['Authorization'], $possibleTokens)) {
-        header('HTTP/1.1 401 Unauthorized');
-        logRequest(__LINE__, 'provided authorization is invalid');
         exit;
     }
 
