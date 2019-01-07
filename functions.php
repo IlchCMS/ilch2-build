ec866fb978cfdc3ba6622d291f8a33aaf618b293
Mairu <mairu@ilch.de>
2019-01-07 22:32:05 +0100

    add apache_request_headers function fallback

--- functions.php
+++ functions.php
@@ -87,3 +87,16 @@ function createArchive($branch, $commit, $tag = null)
         logRequest('Error while creating archive: ' . implode("\n",$cmdOut),__FILE__, __LINE__);
     }
 }
+
+if (!function_exists('apache_request_headers')) {
+    function apache_request_headers()
+    {
+        $headers = array();
+        foreach ($_SERVER as $key => $value) {
+            if (substr($key, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
